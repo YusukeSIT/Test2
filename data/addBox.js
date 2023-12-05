7edb68bbee02b21388e51489d65a5b8b1a4f4979
YusukeSIT <151612445+YusukeSIT@users.noreply.github.com>
2023-12-05 13:24:42 +0900

    Add files via upload

--- data/addBox.js
+++ data/addBox.js
@@ -1,3 +1,5 @@
+let count = 0;
+
 let box_element = document.querySelector('a-box');
 
 let new_element = document.createElement('a-box');
@@ -5,3 +7,13 @@ new_element.setAttribute("position", "0 1 -2");
 new_element.setAttribute("scale", "1 1 1");
 new_element.setAttribute("color", "blue");
 box_element.after(new_element);
+
+clicked() {
+	if (count%2 == 0) {
+		new_element.setAttribute("color", "red");
+	}
+	else {
+		new_element.setAttribute("color", "blue");
+	}
+	count = count + 1;
+}

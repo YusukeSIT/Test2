bdce0dfebe2f4051281144a8d90bb9a0318d47e7
YusukeSIT <151612445+YusukeSIT@users.noreply.github.com>
2023-12-05 12:27:56 +0900

    Delete data/addBox.js

--- data/addBox.js
+++
@@ -1,7 +0,0 @@
-let textbox_element = document.getElementById('world');
-
-let new_element = document.createElement('a-box');
-new_element.setAttribute("position", "0 2 -2");
-new_element.setAttribute("scale", "1 1 1");
-new_element.setAttribute("color", "blue");
-textbox_element.appendChild(new_element);

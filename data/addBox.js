d4910aec04608aece2af8f73d968b7c27f0aa156
YusukeSIT <151612445+YusukeSIT@users.noreply.github.com>
2023-12-05 13:01:03 +0900

    Add files via upload

--- data/addBox.js
+++ data/addBox.js
@@ -1,7 +1,7 @@
-let textbox_element = document.getElementById('world');
+let box_element = document.getElementById('box');
 
 let new_element = document.createElement('a-box');
-new_element.setAttribute("position", "0 0.5 -3");
+new_element.setAttribute("position", "0 2 -2");
 new_element.setAttribute("scale", "1 1 1");
 new_element.setAttribute("color", "blue");
-textbox_element.appendChild(new_element);
+textbox_element.after(new_element);

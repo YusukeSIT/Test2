cca04d30c1cd96ad1185118034729cd693652990
YusukeSIT <151612445+YusukeSIT@users.noreply.github.com>
2023-12-05 13:03:37 +0900

    Update addBox.js

--- data/addBox.js
+++ data/addBox.js
@@ -4,4 +4,4 @@ let new_element = document.createElement('a-box');
 new_element.setAttribute("position", "0 2 -2");
 new_element.setAttribute("scale", "1 1 1");
 new_element.setAttribute("color", "blue");
-textbox_element.after(new_element);
+box_element.after(new_element);
